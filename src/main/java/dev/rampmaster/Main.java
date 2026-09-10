377bd5c4b35069432d457f0f454776f663dd5822
Fary02 <fa.alam@duocuc.cl>
2026-09-10 17:28:59 -0300

    Se instancia la clase estudiante, falta integrar metodo obtenerInformacion

--- src/main/java/dev/rampmaster/Main.java
+++ src/main/java/dev/rampmaster/Main.java
@@ -4,14 +4,17 @@ package dev.rampmaster;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Se instancia la clase estudiante a modo de prueba
+
+        Estudiante estudiante = new Estudiante("Kiwi", "Test", "kitest@example.ex", "12-345-678-9");
+
+        System.out.println("El estudiante: " + estudiante.getNombre() +  " con apellido: " + estudiante.getApellido());
+
+         /* Pendiente integrar metodo obtenerInformacion
+
+         System.out.println(estudiante.obtenerInformacion())
+
+          */
     }
 }
